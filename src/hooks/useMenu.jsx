9ab566352d02691a82c302a8d9ca4da0a4d3f1d2
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-24 15:47:37 +0600

    external server added

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,17 +1,19 @@
+/** @format */
+
 import { useEffect, useState } from "react";
 
 const useMenu = () => {
-    const [menu, setMenu] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        fetch('http://localhost:5000/menu')
-            .then(res => res.json())
-            .then(data => {
-                setMenu(data);
-                setLoading(false);
-            });
-    }, [])
-    return [menu, loading]
-}
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://bistro-boss-server-smoky.vercel.app/menu")
+      .then((res) => res.json())
+      .then((data) => {
+        setMenu(data);
+        setLoading(false);
+      });
+  }, []);
+  return [menu, loading];
+};
 
 export default useMenu;
